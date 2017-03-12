feafb7a59ddcc84c078a2f1cb6d7191896bb2c5b
Luciano Ramalho <luciano@ramalho.org>
2017-03-12 17:41:18 -0300

    mudando caminhos de arquivos importações de módulos em epub.py

--- util/epub.py
+++ util/epub.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python3
+
 from glob import glob
-from shutil import rmtree
-from subprocess import call
-from os import path, makedirs
+import shutil
+import subprocess
+import os
+
+
+URL_REPO = 'https://github.com/PenseAllen/PensePython2e/raw/master'
+
 
 def _project_path():
-    return path.dirname(path.dirname(path.abspath(__file__)))
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def _get_path(p):
-    return path.join(_project_path(), p)
+    return os.path.join(_project_path(), p)
 
 
 def _fix_images_url(files, tmp_dir):
     """ Corrige as urls das imagens.
-        Por algum motivo o pandoc não estava conseguindo baixar as imagens do site para embedar no epub.
-        Com esse código eu utilizo gero arquivos temporários com as essas referências acertadas para a mesma imagem dentro do projeto.
+        Por algum motivo o pandoc não estava conseguindo baixar as imagens
+        do site para embedar no epub. Com esse código eu gero arquivos 
+        temporários com as essas referências acertadas para a mesma imagem
+        dentro do projeto.
     """
-    makedirs(tmp_dir)
+    os.makedirs(tmp_dir)
     fixed_files = []
-    fixed_path = path.relpath(_project_path())
+    fixed_path = os.path.relpath(_project_path())
     for fp in files:
-        file_name = path.split(fp)[1]
-        output = path.join(tmp_dir, file_name)
+        file_name = os.path.split(fp)[1]
+        output = os.path.join(tmp_dir, file_name)
         with open(fp) as input_file:
-            fixed_str = input_file.read().replace('https://github.com/PenseAllen/PensePython2e/raw/master', fixed_path)
+            fixed_str = input_file.read().replace(URL_REPO, fixed_path)
             with open(output, 'w') as output_file:
                 output_file.write(fixed_str)
             fixed_files.append(output)
     return fixed_files
 
 
-
 def main():
     glob_filter = _get_path('docs/*.md')
     capa = _get_path('img/Capa_PenseEmPython332x461-borda.png')
-    metadata = _get_path('docs/metadata.xml')
-    output = _get_path('docs/PenseEmPython.epub')
+    metadata = _get_path('ebooks/metadata.xml')
+    output = _get_path('ebooks/PenseEmPython2e.epub')
     files = glob(glob_filter)
     files.sort()
     files.pop()
@@ -48,9 +54,9 @@ def main():
     args.append('-o')
     args.append(output)
     # Executa o pandoc
-    call(args)
+    subprocess.call(args)
     # Remove arquivos temporários
-    rmtree(tmp_dir)
+    shutil.rmtree(tmp_dir)
     print('Gerado epub em', output)
 
 
